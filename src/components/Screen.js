a80102f7e601c28a2a0bf898857aaeb344e59e95
Alex Mallen <amallen@leadtech.com>
2017-10-24 17:38:07 +0200

    re-order screen.js to funcional component

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -2,12 +2,6 @@ import React from 'react';
 import './Screen.css'
 
 //las props de value de calculator.js
-const Screen = (props) => {
-  return (
-      <div className="screen">
-          {props.value}
-      </div>
-  )
-}
+const Screen = (props) => <div className="screen">{props.value}</div>
 
 export default Screen
